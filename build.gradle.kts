a3dd2d7c56e642282e6f5f8b53112822b247af36
Pavlo Shevchenko <pshevchenko@gradle.com>
2025-02-24 10:58:20 +0100

    Add explicit dependency on the `junit-platform-launcher`

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.12.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
